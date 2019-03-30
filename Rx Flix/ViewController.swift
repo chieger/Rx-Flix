a8a5a69d37a94a542bb6ee36a3eae8ebbe6c4186
chieger <charlie@codepath.com>
2019-03-30 15:22:33 -0700

    Initial Commit

---
+++ Rx Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rx Flix
+//
+//  Created by Charles Hieger on 3/30/19.
+//  Copyright © 2019 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
