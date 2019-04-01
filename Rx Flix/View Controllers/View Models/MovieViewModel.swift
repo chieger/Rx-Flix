8889a48a6a94b1f66bd21c0e2080d4698d713f28
chieger <charlie@codepath.com>
2019-03-31 17:59:14 -0700

    Add movie view model

---
+++ Rx Flix/View Controllers/View Models/MovieViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  MovieViewModel.swift
+//  
+//
+//  Created by Charles Hieger on 3/31/19.
+//
+
+import Foundation
+
+struct MovieViewModel {
+
+   // MARK: - Properties
+
+   let movie: Movie
+
+   private let dateFormatterISO = ISO8601DateFormatter()
+   private let dateFormatter = DateFormatter()
+
+   var title: String {
+      return movie.title
+   }
+
+   var overview: String {
+      return movie.overview
+   }
+
+   var releaseDate: String? {
+      guard let inputDateString = movie.releaseDate else { return nil }
+      guard let date = dateFormatterISO.date(from: inputDateString) else { return nil }
+
+      dateFormatter.dateFormat = "MMM dd"
+      let outputDateString = dateFormatter.string(from: date)
+      return outputDateString
+   }
+
+   var posterImageURL: URL? {
+      guard let posterPath = movie.posterPath, let url = URL(string: API.BaseURLImageString + posterPath) else { return nil }
+         return url  
+   }
+
+   var backdropImageURL: URL? {
+      guard let backdropPath = movie.backdropPath, let url = URL(string: API.BaseURLImageString + backdropPath) else { return nil }
+      return url
+   }
+}
