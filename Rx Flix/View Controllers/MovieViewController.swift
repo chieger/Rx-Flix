d83af604ba33f496405bb28bf8c15a132bd36d72
chieger <charlie@codepath.com>
2019-03-31 20:55:00 -0700

    Refactor table view to use Rx Cocoa Pattern

--- Rx Flix/View Controllers/MovieViewController.swift
+++ Rx Flix/View Controllers/MovieViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AlamofireImage
+import RxSwift
+import RxCocoa
 
 class MovieViewController: UIViewController {
 
@@ -15,11 +17,16 @@ class MovieViewController: UIViewController {
 
    @IBOutlet weak var movieTableView: UITableView!
 
-   var movieViewModels = [MovieViewModel]() {
-      didSet {
-         updateView()
-      }
-   }
+//   var movieViewModels = [MovieViewModel]()
+//   var movieViewModelsRx: Observable<[MovieViewModel]>!
+
+   //   var movieViewModels = [MovieViewModel]() {
+   //      didSet {
+   //         updateView()
+   //      }
+   //   }
+
+   private let disposeBag = DisposeBag()
 
    private lazy var apiManager = {
       return APIManager(baseURL: API.AuthenticatedBaseURL)
@@ -29,18 +36,21 @@ class MovieViewController: UIViewController {
 
    override func viewDidLoad() {
       super.viewDidLoad()
+//      movieViewModelsRx = Observable.just(movieViewModels)
+//      movieViewModelsRx
+//         .bind(to: self.movieTableView.rx.items(cellIdentifier: MovieTableViewCell.reuseIdentifier, cellType: MovieTableViewCell.self)) { (row, movieViewModel, cell) in
+//            cell.configure(withViewModel: movieViewModel)
+//         }
+//         .disposed(by: self.disposeBag)
+
       setupView()
       fetchMoviesNowPlaying()
    }
 
    // MARK: -
    private func setupView() {
-      movieTableView.dataSource = self
+      // movieTableView.dataSource = self
       movieTableView.rowHeight = 200
-   }
-
-   private func updateView() {
-      movieTableView.reloadData()
    }
 
    // MARK: - Helper Methods
@@ -51,25 +61,33 @@ class MovieViewController: UIViewController {
             return
          } else if let movies = movies {
             // Create view models from model
-            self.movieViewModels = movies.map({ (movie) -> MovieViewModel in
+            let movieViewModels = movies.map({ (movie) -> MovieViewModel in
                return MovieViewModel(movie: movie)
             })
+            // Create Rx view models
+            let movieViewModelsRx = Observable.just(movieViewModels)
+            // Bind Rx view model to table view
+            movieViewModelsRx
+               .bind(to: self.movieTableView.rx.items(cellIdentifier: MovieTableViewCell.reuseIdentifier, cellType: MovieTableViewCell.self)) { (row, movieViewModel, cell) in
+                  cell.configure(withViewModel: movieViewModel)
+               }
+               .disposed(by: self.disposeBag)
          }
       }
    }
 }
 
-extension MovieViewController: UITableViewDataSource {
-
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return movieViewModels.count
-   }
-
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.reuseIdentifier, for: indexPath) as? MovieTableViewCell else { fatalError("Unexpected Table View Cell")}
-      let movieViewModel = movieViewModels[indexPath.row]
-      cell.configure(withViewModel: movieViewModel)
-      return cell
-   }
-}
+//extension MovieViewController: UITableViewDataSource {
+//
+//   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//      return movieViewModels.count
+//   }
+//
+//   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//      guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.reuseIdentifier, for: indexPath) as? MovieTableViewCell else { fatalError("Unexpected Table View Cell")}
+//      let movieViewModel = movieViewModels[indexPath.row]
+//      cell.configure(withViewModel: movieViewModel)
+//      return cell
+//   }
+//}
 
